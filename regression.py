5038a7bcd631504651e66466258fddbf7308ccb1
Ceyda Gönülalçak <ceydagonulalcakcg@gmail.com>
2023-09-09 19:40:34 +0300

    Update regression.py

--- regression.py
+++ regression.py
@@ -4,77 +4,71 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.linear_model import LinearRegression
 import numpy as np
 
-df = pd.read_excel("ML1/data.xlsx", sheet_name="reg")
+df = pd.read_excel("CaseStudy1/data.xlsx", sheet_name="reg")
 df.head()
 
 
-### Maaş Tahmin ###
+### Salary Prediction ###
 # y' = b + wx
 # b = 275
 # w = 90
+
 def my_linear_regression(x, bias=275, weight=90):
     return bias + (weight * x)
 
-df["Maaş Tahmin"] = df["Deneyim Yılı"].apply(my_linear_regression)
+df["Salary Prediction"] = df["Years of Experience"].apply(my_linear_regression)
 
-df["Maaş Tahmin"]
+df["Salary Prediction"]
 
 
-### Hata ###
+### Error ###
+
 def my_error(y, yi):
     return y - yi
 
-df["Hata"] = df[["Maaş", "Maaş Tahmin"]].apply(lambda x: my_error(x[0], x[1]), axis=1)
+df["Error"] = df[["Salary", "Salary Prediction"]].apply(lambda x: my_error(x[0], x[1]), axis=1)
 
-df["Hata"]
+df["Error"]
 
 
-### Hata Kareler ###
+### Error Squares ###
+
 def my_root_error(y, yi):
     return (y - yi) ** 2
 
-df["Hata Kareler"] = [my_root_error(row["Maaş"], row["Maaş Tahmin"]) for idx,row in df.iterrows()]
+df["Error Squares"] = [my_root_error(row["Salary"], row["Salary Prediction"]) for idx,row in df.iterrows()]
 
-df["Hata Kareler"]
+df["Error Squares"]
 
 
-### Mutlak Hata ###
+### Absolute Error ###
 
-df["Mutlak Hata"] = abs(df["Maaş Tahmin"] - df["Maaş"])
+df["Absolute Error"] = abs(df["Salary Prediction"] - df["Salary"])
 
-df["Mutlak Hata"]
-
+df["Absolute Error"]
 
 
 #mse
-mse = sum(df["Hata"] ** 2) / len(df)
-mse
+mse = sum(df["Error"] ** 2) / len(df)
 
 #rmse
 rmse = math.sqrt(mse)
-rmse
 
 #mae
-mae = sum(abs(df["Hata"])) / len(df)
-mae
+mae = sum(abs(df["Error"])) / len(df)
 
 
+mean_squared_error(df["Salary"], df["Salary Prediction"])
+math.sqrt(mean_squared_error(df["Salary"], df["Salary Prediction"]))
+mean_absolute_error(df["Salary"], df["Salary Prediction"])
 
-
-mean_squared_error(df["Maaş"], df["Maaş Tahmin"])
-math.sqrt(mean_squared_error(df["Maaş"], df["Maaş Tahmin"]))
-mean_absolute_error(df["Maaş"], df["Maaş Tahmin"])
-
-
-
-################################################
 
 df.head()
-lin = LinearRegression().fit(df[["Deneyim Yılı"]], df[["Maaş"]])
+lin = LinearRegression().fit(df[["Years of Experience"]], df[["Salary"]])
 lin.coef_
 lin.intercept_
 
 
-np.inner(lin.coef_, df[["Deneyim Yılı"]].iloc[0,:]) + lin.intercept_
+np.inner(lin.coef_, df[["Salary Prediction"]].iloc[0,:]) + lin.intercept_
 df.iloc[0,:]
 
